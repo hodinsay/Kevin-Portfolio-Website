7e6c6e9c7624ea260fb7c819b77d74800a10de2a
Din Ho <100463706+hodinsay@users.noreply.github.com>
2022-08-05 13:37:57 -0700

    second commit
    Added nav menu slider function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+// Nav Menu Slider
 
+const navMenu = document.querySelector(".nav-menu");
+const closeMenu = document.querySelector(".close-menu");
+const openMenu = document.querySelector(".open-menu");
+
+navMenu.style.right = "-200px"
+
+openMenu.addEventListener("click", () => {
+    navMenu.style.right = "0";
+});
+
+closeMenu.addEventListener("click", () => {
+    navMenu.style.right = "-200px";
+});
